22efb8b1f05958bd34fabf8c416b5063b72c1f77
= <=>
2017-02-09 18:39:25 +0800

    init project

---
+++ src/Help.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: xiaofang
+ * Date: 2017/2/9
+ * Time: 下午6:32
+ */
+class Help
+{
+
+    public function index()
+    {
+        echo 'xiaofang';
+    }
+}
